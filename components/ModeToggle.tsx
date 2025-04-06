37ec90d4d98c622ec631c3525fb0d4c058e8f59a
MisaJC <123918601+JCMisa@users.noreply.github.com>
2025-04-06 11:39:08 +0000

    use magic ui dock component as a navbar menu

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -5,7 +5,7 @@ import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
-export default function ModeToggle() {
+export default function ModeToggle({ className = "" }: { className?: string }) {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = React.useState(false);
 
@@ -20,6 +20,7 @@ export default function ModeToggle() {
       variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className={`${className}`}
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
